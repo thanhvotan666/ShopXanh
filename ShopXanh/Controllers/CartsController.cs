867f1294ec309439ff0bfa972456d733cad41c96
thanhvotan666 <thanhvotan666@gmail.com>
2024-03-31 18:34:28 +0700

    31/3

---
+++ ShopXanh/Controllers/CartsController.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ShopXanh.Data;
+using ShopXanh.Models;
+
+namespace ShopXanh.Controllers
+{
+    public class CartsController : Controller
+    {
+        private readonly ShopXanhContext _context;
+
+        public CartsController(ShopXanhContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Carts
+        public async Task<IActionResult> Index()
+        {
+            var shopXanhContext = _context.Cart.Include(c => c.nguoiDung).Include(c => c.sanPham);
+            return View(await shopXanhContext.ToListAsync());
+        }
+
+        // GET: Carts/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cart = await _context.Cart
+                .Include(c => c.nguoiDung)
+                .Include(c => c.sanPham)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (cart == null)
+            {
+                return NotFound();
+            }
+
+            return View(cart);
+        }
+
+        // GET: Carts/Create
+        public IActionResult Create()
+        {
+            ViewData["NguoiDungId"] = new SelectList(_context.NguoiDung, "Id", "Id");
+            ViewData["SanPhamId"] = new SelectList(_context.SanPham, "Id", "Id");
+            return View();
+        }
+
+        // POST: Carts/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Quantity,SanPhamId,NguoiDungId")] Cart cart)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(cart);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["NguoiDungId"] = new SelectList(_context.NguoiDung, "Id", "Id", cart.NguoiDungId);
+            ViewData["SanPhamId"] = new SelectList(_context.SanPham, "Id", "Id", cart.SanPhamId);
+            return View(cart);
+        }
+
+        // GET: Carts/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cart = await _context.Cart.FindAsync(id);
+            if (cart == null)
+            {
+                return NotFound();
+            }
+            ViewData["NguoiDungId"] = new SelectList(_context.NguoiDung, "Id", "Id", cart.NguoiDungId);
+            ViewData["SanPhamId"] = new SelectList(_context.SanPham, "Id", "Id", cart.SanPhamId);
+            return View(cart);
+        }
+
+        // POST: Carts/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Quantity,SanPhamId,NguoiDungId")] Cart cart)
+        {
+            if (id != cart.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(cart);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CartExists(cart.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["NguoiDungId"] = new SelectList(_context.NguoiDung, "Id", "Id", cart.NguoiDungId);
+            ViewData["SanPhamId"] = new SelectList(_context.SanPham, "Id", "Id", cart.SanPhamId);
+            return View(cart);
+        }
+
+        // GET: Carts/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var cart = await _context.Cart
+                .Include(c => c.nguoiDung)
+                .Include(c => c.sanPham)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (cart == null)
+            {
+                return NotFound();
+            }
+
+            return View(cart);
+        }
+
+        // POST: Carts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var cart = await _context.Cart.FindAsync(id);
+            if (cart != null)
+            {
+                _context.Cart.Remove(cart);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CartExists(int id)
+        {
+            return _context.Cart.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ ShopXanh/Controllers/HoaDonsController.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ShopXanh.Data;
+using ShopXanh.Models;
+
+namespace ShopXanh.Controllers
+{
+    public class HoaDonsController : Controller
+    {
+        private readonly ShopXanhContext _context;
+
+        public HoaDonsController(ShopXanhContext context)
+        {
+            _context = context;
+        }
+
+        // GET: HoaDons
+        public async Task<IActionResult> Index()
+        {
+            var shopXanhContext = _context.HoaDon.Include(h => h.NguoiDung);
+            return View(await shopXanhContext.ToListAsync());
+        }
+
+        // GET: HoaDons/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var hoaDon = await _context.HoaDon
+                .Include(h => h.NguoiDung)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (hoaDon == null)
+            {
+                return NotFound();
+            }
+
+            return View(hoaDon);
+        }
+
+        // GET: HoaDons/Create
+        public IActionResult Create()
+        {
+            ViewData["NguoiDungId"] = new SelectList(_context.NguoiDung, "Id", "Id");
+            return View();
+        }
+
+        // POST: HoaDons/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,TotalPayment,Address,NguoiDungId")] HoaDon hoaDon)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(hoaDon);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["NguoiDungId"] = new SelectList(_context.NguoiDung, "Id", "Id", hoaDon.NguoiDungId);
+            return View(hoaDon);
+        }
+
+        // GET: HoaDons/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var hoaDon = await _context.HoaDon.FindAsync(id);
+            if (hoaDon == null)
+            {
+                return NotFound();
+            }
+            ViewData["NguoiDungId"] = new SelectList(_context.NguoiDung, "Id", "Id", hoaDon.NguoiDungId);
+            return View(hoaDon);
+        }
+
+        // POST: HoaDons/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,TotalPayment,Address,NguoiDungId")] HoaDon hoaDon)
+        {
+            if (id != hoaDon.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(hoaDon);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!HoaDonExists(hoaDon.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["NguoiDungId"] = new SelectList(_context.NguoiDung, "Id", "Id", hoaDon.NguoiDungId);
+            return View(hoaDon);
+        }
+
+        // GET: HoaDons/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var hoaDon = await _context.HoaDon
+                .Include(h => h.NguoiDung)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (hoaDon == null)
+            {
+                return NotFound();
+            }
+
+            return View(hoaDon);
+        }
+
+        // POST: HoaDons/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var hoaDon = await _context.HoaDon.FindAsync(id);
+            if (hoaDon != null)
+            {
+                _context.HoaDon.Remove(hoaDon);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool HoaDonExists(int id)
+        {
+            return _context.HoaDon.Any(e => e.Id == id);
+        }
+    }
+}
